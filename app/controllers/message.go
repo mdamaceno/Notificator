6b552dc9b0052402f188093d406e2b1ef1a962ce
Marco Damaceno <maadamaceno@gmail.com>
2024-01-10 17:54:45 -0300

    chore: prepare route for message

---
+++ app/controllers/message.go
@@ -0,0 +1,9 @@
+package controllers
+
+import "github.com/labstack/echo"
+
+type MessageController struct{}
+
+func (c MessageController) Create(ctx echo.Context) error {
+	return nil
+}
--- app/routes.go
+++ app/routes.go
@@ -2,8 +2,15 @@ package app
 
 import (
 	"github.com/labstack/echo"
+	"github.com/mdmaceno/notificator/app/controllers"
 	"gorm.io/gorm"
 )
 
 func InitRoutes(e *echo.Echo, DB *gorm.DB) *echo.Echo {
+	messageController := controllers.MessageController{}
+	api := e.Group("/api")
+
+	api.POST("/message", messageController.Create)
+
+	return e
 }

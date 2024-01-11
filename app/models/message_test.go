fdd6892ba8e6d80be25dbf094cc2b1c9af98b0aa
Marco Damaceno <maadamaceno@gmail.com>
2024-01-11 20:03:55 -0300

    test: model message methods and functions

---
+++ app/models/message_test.go
@@ -0,0 +1,56 @@
+package models
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMessageModel(t *testing.T) {
+	t.Run("NewMessage", func(t *testing.T) {
+		t.Run("should return message when message params is not nil", func(t *testing.T) {
+			im := &IncomingMessage{
+				Service:   "email",
+				Title:     "title",
+				Body:      "body",
+				SendAt:    "2021-01-01T00:00:00Z",
+				Receivers: MessageReceivers{"john@email.com", "doe@email.com"},
+			}
+
+			message, err := NewMessage(im)
+			timeParsed, _ := time.Parse(time.RFC3339, im.SendAt)
+
+			assert.Nil(t, err)
+			assert.Equal(t, im.Service, message.Service)
+			assert.Equal(t, im.Title, message.Title)
+			assert.Equal(t, im.Body, message.Body)
+			assert.Equal(t, &timeParsed, message.SendAt)
+			for i, destination := range message.Destinations {
+				assert.Equal(t, im.Receivers[i], destination.Receiver)
+			}
+		})
+
+		t.Run("should return error when message params is nil", func(t *testing.T) {
+			_, err := NewMessage(nil)
+
+			assert.NotNil(t, err)
+		})
+	})
+
+	t.Run("FilterEmails", func(t *testing.T) {
+		t.Run("should return emails when message has destinations", func(t *testing.T) {
+			message := Message{
+				Destinations: []Destination{
+					{Receiver: "john@email.com"},
+					{Receiver: "+628123456789"},
+				},
+			}
+
+			emails := message.FilterEmails()
+
+			assert.Equal(t, 1, len(emails))
+			assert.Equal(t, message.Destinations[0].Receiver, emails[0])
+		})
+	})
+}

38ba403ea905c8261be3f34d4231f655853e8f3e
Marco Damaceno <maadamaceno@gmail.com>
2024-01-11 20:02:41 -0300

    refactor: flat title and body in message

--- app/models/message.go
+++ app/models/message.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"errors"
 	"time"
 
 	"github.com/google/uuid"
@@ -17,17 +18,59 @@ var Services = struct {
 type Message struct {
 	Id        uuid.UUID  `gorm:"type:uuid;primary_key;default:gen_random_uuid()"`
 	Service   string     `gorm:"type:varchar(255);not null"`
-	Payload   Payload    `gorm:"type:jsonb;not null;serializer:json"`
+	Title     string     `gorm:"type:varchar(255);not null"`
+	Body      string     `gorm:"type:text;not null"`
 	SendAt    *time.Time `gorm:"type:timestamp;"`
 	CreatedAt time.Time  `gorm:"not null;"`
 	UpdatedAt time.Time  `gorm:"not null;"`
+
+	Destinations []Destination
 }
 
-type MessageReceiver []string
+type MessageReceivers []string
 
 type IncomingMessage struct {
-	Service   string          `json:"service" validate:"required"`
-	Payload   IncomingPayload `json:"payload" validate:"required"`
-	SendAt    string          `json:"send_at" validate:"datetime,omitempty"`
-	Receivers MessageReceiver `json:"receivers" validate:"required"`
+	Service   string           `json:"service" validate:"required"`
+	Title     string           `json:"title" validate:"required"`
+	Body      string           `json:"body" validate:"required"`
+	SendAt    string           `json:"send_at" validate:"datetime,omitempty"`
+	Receivers MessageReceivers `json:"receivers" validate:"required"`
+}
+
+func NewMessage(im *IncomingMessage) (Message, error) {
+	messageId := uuid.New()
+
+	if im == nil {
+		return Message{}, errors.New("message params is nil")
+	}
+
+	message := Message{
+		Id:      messageId,
+		Service: im.Service,
+		Title:   im.Title,
+		Body:    im.Body,
+	}
+
+	if im.SendAt != "" {
+		sendAt, err := time.Parse(time.RFC3339, im.SendAt)
+
+		if err != nil {
+			return Message{}, err
+		}
+
+		message.SendAt = &sendAt
+	}
+
+	destinations := make([]Destination, len(im.Receivers))
+	for i, receiver := range im.Receivers {
+		destinations[i] = Destination{
+			MessageId: messageId,
+			Receiver:  receiver,
+		}
+	}
+
+	message.Destinations = destinations
+
+	return message, nil
+}
 }
